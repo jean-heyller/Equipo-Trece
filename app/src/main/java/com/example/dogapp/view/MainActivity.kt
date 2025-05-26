9015fe0b8ad37e6ff4b392cc7e5c623210789a02
jean palomino <118295259+jean-heyller@users.noreply.github.com>
2025-05-25 23:30:29 -0500

    se agrega las carpetas base para la arquitectura mvvm

--- app/src/main/java/com/example/dogapp/view/MainActivity.kt
+++ app/src/main/java/com/example/dogapp/view/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.example.dogapp
+package com.example.dogapp.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.dogapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
